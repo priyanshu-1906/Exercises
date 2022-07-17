72de28f4eb272dcf56784bf9d6bf9f7039260eb3
priyanshu-1906 <89680386+priyanshu-1906@users.noreply.github.com>
2022-07-17 13:56:17 +0530

    Add files via upload

---
+++ input_function.py
@@ -0,0 +1,11 @@
+#Write your code beloprint
+# -->print("hello " + input(" what is your name "))
+# int the above it will first ask your name and then it will execute and write hello priyanshu or any other name as per the input
+#ans for the following exercize is below
+
+a = input("What is your name?")
+print(len(a))
+
+#or 
+
+ print(len(input("what is your name?")))
