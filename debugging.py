6ca9bfe2662ede24c2a70bfa8e316a20aff26dd0
priyanshu-1906 <89680386+priyanshu-1906@users.noreply.github.com>
2022-07-17 13:50:59 +0530

    Add files via upload

---
+++ debugging.py
@@ -0,0 +1,10 @@
+#Fix the code below 👇
+
+print("Day 1 - String Manipulation")
+# if you want to show double quotes inside the statement which you are about to print then use single quote inside print as shown below like this you can show double quotes 
+
+print('String Concatenation is done with the "+" sign.')
+# another note you cannot use double quotes two times that's why we have used single quotes
+
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
