ad2e75cc95b2c41320393e134e071d6636aeecfc
priyanshu-1906 <89680386+priyanshu-1906@users.noreply.github.com>
2022-07-17 15:31:45 +0530

    Add files via upload

---
+++ Datatypes.py
@@ -0,0 +1,14 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+####################################
+#Write your code below this line 👇
+# to check which datatype is it if you are not getting     desired result then write 
+#######################################################
+#-----------> print(type(two_digit_number)) <--------------
+#######################################################
+# like this you can check what type it is like in this case it was string hence we changed it to string and then performed the following addition operation
+
+########################################################
+
+print(int (two_digit_number[0]) + int (two_digit_number[1]))
